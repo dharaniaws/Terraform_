7864a1667529d691ccdf205098fbe0f98ac46eb0
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-08-14 14:17:37 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "subnet-cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "subnet1-cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "subnet2-cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "subnet3-cidr" {
+  default = "10.0.4.0/24"
+}
+
+variable "subnet4-cidr" {
+  default = "10.0.5.0/24"
+}
+
+variable "subnet5-cidr" {
+  default = "10.0.6.0/24"
+}
