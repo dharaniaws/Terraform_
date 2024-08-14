595de9ae86f7e44d7030a014e0a4cc3869fc2d82
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-08-14 14:26:29 +0530

    Create web_sg.tf

---
+++ web_sg.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "demosg" {
+  vpc_id = aws_vpc.demovpc.id
+  #inbound rules
+  #http access from any where
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #http access from any where
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #ssh access from any where
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #outbound rules
+  #internet access to anywhere
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "websg"
+  }
+}
